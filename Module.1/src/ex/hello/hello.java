86bbce9a61a4701953fe2dedb6869bc649a12a36
anhtuan9 <42605422+anhtuan9@users.noreply.github.com>
2018-10-03 09:49:17 +0700

    hello

---
+++ Module.1/src/ex/hello/hello.java
@@ -0,0 +1,13 @@
+package ex.hello;
+
+import java.util.Scanner;
+
+public class hello {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = input.nextLine();
+
+        System.out.println("Hello: " + name);
+    }
+}
