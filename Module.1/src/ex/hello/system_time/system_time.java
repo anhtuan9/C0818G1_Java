97cd1082b2a0296079d68d0435b7cad0cb8ff712
anhtuan9 <42605422+anhtuan9@users.noreply.github.com>
2018-10-03 10:46:00 +0700

    done

---
+++ Module.1/src/ex/hello/system_time/system_time.java
@@ -0,0 +1,10 @@
+package ex.hello.system_time;
+
+import java.util.Date;
+
+public class system_time {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is: " + now);
+    }
+}
